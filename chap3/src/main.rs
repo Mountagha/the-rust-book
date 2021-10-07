a22e5ebb3f2c2ccf489e0f5439dda0a6945cbe9a
Mountagha <muntaghaba@gmail.com>
2021-10-06 21:41:58 -0400

    finished chap3

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {}", x);
-    x = 6;
-    println!("value changed to {}", x);
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is : {}", number);
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2
+        }
+    };
+    println!("result is {} and counter is {}", result, counter);
+
+    /* another kind of loop */
+    for elt in (1..4).rev() {
+        println!("element : {}", elt);
+    }
+
 }
+
 
