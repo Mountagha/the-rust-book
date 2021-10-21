d71b5bf1eda78d253412d01222d4d381a45d500e
Mountagha <muntaghaba@gmail.com>
2021-10-20 23:29:32 -0400

    on generic & traits

--- chap10/src/main.rs
+++ chap10/src/main.rs
@@ -1,3 +1,18 @@
+fn larges<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest{
+            largest = item;
+        }
+    }
+    largest;
+}
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![10, 40, 5, 100, 80];
+    let result = largest(&number_list)
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['c', 'a', 'x', 'b'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
