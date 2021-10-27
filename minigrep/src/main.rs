968d2fd518457b1c3cc715e21ad98584b58c15c9
Mountagha <muntaghaba@gmail.com>
2021-10-26 22:43:56 -0400

    on minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (query, filename) = parse_config(&args);
+    let content = fs::read_to_string(filename).expect(
+        "Something went wrong reading the file"
+    );
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
+
+
+
