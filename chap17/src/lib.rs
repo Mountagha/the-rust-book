f2a4ec3287680103eee4f12d4d4f82c910b72dd5
Mountagha <muntaghaba@gmail.com>
2021-12-07 23:49:15 -0500

    on blog. almost done.

--- chap17/src/lib.rs
+++ chap17/src/lib.rs
@@ -104,4 +104,18 @@ mod tests {
         aC.add(2); 
         assert_eq!(aC.average(), 3.0);
     }
+
+    // test for blog
+    fn test_draft() {
+        let mut post = Post::new();
+
+        post.add_text("I ate a salad for lunch today");
+        assert_eq!("", post.content());
+
+        post.request_review();
+        assert_eq!("", post.content());
+
+        post.approve();
+        assert_eq!("I ate a salad for lunch today", post.content());
+    }
 }
