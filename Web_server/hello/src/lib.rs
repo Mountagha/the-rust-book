b432582f947888a514a699bcfb117d3020d3b5d8
Mountagha <muntaghaba@gmail.com>
2021-12-18 23:55:27 -0500

    on multithreading the web serv

--- Web_server/hello/src/lib.rs
+++ Web_server/hello/src/lib.rs
@@ -1,6 +1,27 @@
+use std::thread;
+use std::sync::mpsc;
 
-pub struct ThreadPool;
 
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Worker {
+    id: usize;
+    thread: thread::JoinHandle<()>;
+}
+
+struct Job;
+
+impl Worker {
+    fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+        let thread = thread::std::thread::spawn(|| {
+            receiver;
+        });
+        Worker {id, thread}
+    }
+}
 impl ThreadPool {
     /// Create a new ThreadPool.
     /// 
@@ -10,7 +31,16 @@ impl ThreadPool {
     /// 
     /// The `new` function will panic if the size is zero
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+        
+        let (sender, receiver) = mpsc::channel();
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver));
+        }
+        ThreadPool { workers, sender }
     }
     
     pub fn execute<F>(&self, f: F)
