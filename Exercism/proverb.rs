7a67a1db44227d08b38e12c2545ab1f6e85b0d73
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-08-21 23:49:04 -0400

    adding proverb

--- Exercism/proverb.rs
+++ Exercism/proverb.rs
@@ -1,3 +1,5 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
     if list.len() < 1 {
         return String::new();
@@ -8,6 +10,16 @@ pub fn build_proverb(list: &[&str]) -> String {
     }
     phrases.push(format!("And all for the want of a {}.", list[0]));
     phrases.join("\n")
+}
+
+pub fn build_proverb(list: &[&str]) -> String {
+    match list.first() {
+        None => String::new(),
+        Some(word) => list.windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
+    }
 }
 
 #[test]
